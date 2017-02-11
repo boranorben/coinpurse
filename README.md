5396c40b626c55033a121f209bb75484edddb335
Issaree Srisomboon <issaree.sr@ku.th>
2017-02-11 16:33:10 +0700

    first commit in coinpurse

---
+++ README.md
@@ -0,0 +1,31 @@
+# CoinPurse by Issaree Srisomboon
+# Description of coinpurse
+CoinPurse is an object that users can insert coins in the purse and also withdraw from its conditionally.
+In Coin class, it represents coinage (money) with a fixed value and currency.
+In Purse class, it is reponsible for insert coins, withdraw money, check the balance and check if the purse is full.
+In Main class, it will ask users to deposit, withdraw, inquiry or quit.
+## Example methods in Purse class
+| Method | Short Description |
+| -----: | -----------------:
+| `.getBalance()` |  Get the total value in Purse.
+| `.getCapacity()` | Return the capacity of the coin purse.
+| `.isfull()` | Test whether the purse is full.
+| `.insert(Coin coin)` |  Insert a coin into the purse.
+| `.withdraw(double amount)` | Withdraw the requested amount of money.
+# Results in CoinUtil class
+There are 3 example methods in this class,
+`.filterByCurrency(final List<Coin> coinlist, String currency)`
+*Method that examines all the coins in a List and returns only the coins that have a currency that matches the parameter value.*
+- **Input**: 0.25-Baht 0.50-Rupee 50.00-Ringgit 5.00-Baht 1.00-Ringgit 1.00-Rupee 1.00-Baht 2.00-Ringgit 10.00-Baht 0.50-Rupee 10.00-Baht 10.00-Rupee 2.00-Baht 5.00-Ringgit
+- **Output**: 0.50-Rupee 1.00-Rupee 0.50-Rupee 10.00-Rupee 
+
+`.sortByCurrency(List<Coin> coins)`
+*Method to sort a list of coins by currency.*
+- **Input**: 10.00-Rupee 5.00-Ringgit 10.00-Baht 0.25-Baht 0.50-Rupee 2.00-Ringgit 1.00-Baht 1.00-Rupee 5.00-Baht 1.00-Ringgit 0.50-Rupee 50.00-Ringgit 2.00-Baht 10.00-Baht
+- **Output**: 10.00-Baht 0.25-Baht 1.00-Baht 5.00-Baht 2.00-Baht 10.00-Baht 5.00-Ringgit 2.00-Ringgit 1.00-Ringgit 50.00-Ringgit 10.00-Rupee 0.50-Rupee 1.00-Rupee 0.50-Rupee
+
+`.sumByCurrency(List<Coin> coins)`
+*Sum coins by currency and print the sum for each currency.*
+- **Input**: 0.25-Baht 2.00-Baht 1.00-Ringgit 0.50-Rupee 0.50-Rupee 5.00-Ringgit 2.00-Ringgit 5.00-Baht 10.00-Baht 10.00-Rupee 50.00-Ringgit 1.00-Rupee 10.00-Baht 1.00-Baht
+- **Output**: 28.25 Baht
+58.0 Ringgit
---
+++ src/coinpurse/Purse.java
@@ -0,0 +1,138 @@
+package coinpurse;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ *  A coin purse contains coins.
+ *  You can insert coins, withdraw money, check the balance,
+ *  and check if the purse is full.
+ *  When you withdraw money, the coin purse decides which
+ *  coins to remove.
+ *  
+ *  @author Issaree Srisomboon
+ */
+public class Purse {
+	
+    /** Collection of objects in the purse. */
+    private List<Coin> money;
+    
+    /** Capacity is maximum number of coins the purse can hold.
+     *  Capacity is set when the purse is created and cannot be changed.
+     */
+    private final int capacity;
+    
+    /** 
+     *  Create a purse with a specified capacity.
+     *  @param capacity is maximum number of coins you can put in purse.
+     */
+    public Purse( int capacity ) {
+    	this.capacity = capacity;
+    	this.money = new ArrayList<Coin>();
+    }
+
+    /**
+     * Count and return the number of coins in the purse.
+     * This is the number of coins, not their value.
+     * @return the number of coins in the purse
+     */
+    public int count() {
+    	return this.money.size();
+    }
+    
+    /** 
+     *  Get the total value of all items in the purse.
+     *  @return the total value of items in the purse.
+     */
+    public double getBalance() {
+    	double balance=0.0;
+    	for (int i=0 ; i<money.size() ; i++) {
+    		balance += money.get(i).getValue();
+    	}
+    	return balance;
+    }
+    
+    /**
+     * Return the capacity of the coin purse.
+     * @return the capacity
+     */
+    public int getCapacity() {
+    	return this.capacity;
+    }
+    
+    /** 
+     *  Test whether the purse is full.
+     *  The purse is full if number of items in purse equals
+     *  or greater than the purse capacity.
+     *  @return true if purse is full.
+     */
+    public boolean isFull() {
+        //TODO complete this method
+        //TODO Don't Repeat Yourself: Avoid writing duplicate code.
+    	if(this.count() == this.capacity) return true;
+        return false;
+    }
+
+    /** 
+     * Insert a coin into the purse.
+     * The coin is only inserted if the purse has space for it
+     * and the coin has positive value.  No worthless coins!
+     * @param coin is a Coin object to insert into purse
+     * @return true if coin inserted, false if can't insert
+     */
+    public boolean insert( Coin coin ) {
+        // if the purse is already full then can't insert anything.
+        //TODO complete the insert method
+    	if (!isFull() && coin.getValue() > 0) {
+    		money.add(coin);
+    		Collections.sort(money);
+    		Collections.reverse(money);
+			return true;
+    	}
+        return false;
+    }
+    
+    /**  
+     *  Withdraw the requested amount of money.
+     *  Return an array of Coins withdrawn from purse,
+     *  or return null if cannot withdraw the amount requested.
+     *  @param amount is the amount to withdraw
+     *  @return array of Coin objects for money withdrawn, 
+	 *    or null if cannot withdraw requested amount.
+     */
+    public Coin[] withdraw( double amount ) {
+    	List<Coin> templist = new ArrayList<Coin>();
+		if (amount > 0) {	
+			for (int i=0 ; i<=money.size()-1 ; i++) {
+				double coins = money.get(i).getValue();
+				if (coins <= amount) {
+					amount -= coins;
+					templist.add(money.get(i));
+					money.remove(money.get(i));
+				}
+			}
+			if (amount!=0) {
+				for (Coin withdraw : templist) {
+					money.add(withdraw);
+					Collections.sort(money);
+					Collections.reverse(money);
+				}
+				return null;
+			}
+		}
+		Coin[] array = new Coin[templist.size()];
+		templist.toArray(array);
+		return array;
+	}
+  
+    /** 
+     * toString returns a string description of the purse contents.
+     * @return a string description of the purse contents.
+     */
+    public String toString() {
+    	return String.format("%d coins with value %.1f",this.count(),getBalance());
+    }
+
+}
